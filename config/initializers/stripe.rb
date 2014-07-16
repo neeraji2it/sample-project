f809a6a9b6891b2299130cceb440fddf2b6c0f49
rails <rails@rails.(none)>
2014-07-17 00:20:07 +0530

    key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key      = "sk_live_4P1RWBYk2AJnFmmaXRP1XP7F"
-STRIPE_PUBLIC_KEY   = "pk_live_4P1RqjXBQPAOd5PDn5WRZUC4"
+Stripe.api_key      = "sk_live_4PPzrftXE20Yh2xMTam9hIKJ"
+STRIPE_PUBLIC_KEY   = "pk_live_4PPz9siLIHEdcS8YxnUmrMyl"
